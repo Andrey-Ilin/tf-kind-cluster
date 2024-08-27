4d8bd3941bba2af3cffa9655084afc1b73be408d
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-08-27 07:45:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    kind = {
-      source  = "tehcyx/kind"
-      version = "0.2.1"
-    }
-    flux = {
-      source  = "fluxcd/flux"
-      version = "1.0.0-rc.3"
-    }
-  }
-}
-
 resource "kind_cluster" "this" {
   name = "kind-cluster"
   config = <<-EOF
