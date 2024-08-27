4088aa3a3d788d98132dcc744de95ea7ba17055d
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-08-27 07:40:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-provider "kind" {
-  provider   = "docker"
-  kubeconfig = pathexpand("${path.module}/kind-config")
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "0.2.1"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = "1.0.0-rc.3"
+    }
+  }
 }
+
+provider "kind" {}
 
 resource "kind_cluster" "this" {
   name = "kind-cluster"
