12ce1734acc51cc6e28bb0aa6fb6cbc9f9410b2c
Andrii Ilin <andrey.ilyin83@gmail.com>
2024-08-27 09:04:59 -0600

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     kind = {
-      source  = "tehcyx/kind"
-      version = "0.2.1"
+      source = "justenwalker/kind"
+      version = "0.17.0"
     }
     flux = {
       source  = "fluxcd/flux"
